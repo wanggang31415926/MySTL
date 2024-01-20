670809f1decc2c632a028e7afaeee3163d97dff9
wanggwang <gangwang1014@163.com>
2024-01-20 15:16:03 +0800

    add algobase.h modify util.h

---
+++ algobase.h
@@ -0,0 +1,34 @@
+#ifndef MY_STL_ALGOBASE_H_
+#define MY_STL_ALGOBASE_H_
+
+//实现mystl 的基本算法
+
+#include <cstring>
+#include "iterator.h"
+#include "util.h"
+
+namespace mystl
+{
+
+
+#ifdef max
+#pragma message("#undefing marco max")
+#undef max
+#endif // max
+
+#ifdef min
+#pragma message("#undefing marco min")
+#undef min
+#endif // min
+
+
+/*****************************************************************************************/
+// max
+// 取二者中的较大值，语义相等时保证返回第一个参数
+/*****************************************************************************************/
+
+
+
+}
+#endif // !MY_STL_ALGOBASE_H_
+
